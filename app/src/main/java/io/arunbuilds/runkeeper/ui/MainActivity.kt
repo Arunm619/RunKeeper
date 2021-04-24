ef3e2c97c6f77c52dcf79c9ea5f2663f22b5ee9f
Arun Sudharsan <sudarun@amazon.com>
2021-04-24 17:56:12 +0530

    Setting up navigation

--- app/src/main/java/io/arunbuilds/runkeeper/ui/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/runkeeper/ui/MainActivity.kt
@@ -1,14 +1,30 @@
 package io.arunbuilds.runkeeper.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.arunbuilds.runkeeper.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    in arrayOf(R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment) ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/io/arunbuilds/runkeeper/ui/fragments/SetupFragment.kt
+++ app/src/main/java/io/arunbuilds/runkeeper/ui/fragments/SetupFragment.kt
@@ -3,20 +3,18 @@ package io.arunbuilds.runkeeper.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.arunbuilds.runkeeper.R
-import io.arunbuilds.runkeeper.ui.videmodels.MainViewModel
-import kotlinx.android.synthetic.main.fragment_run.*
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
-    private val viewModel: MainViewModel by viewModels()
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fab.setOnClickListener {
-            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
         }
     }
 }
